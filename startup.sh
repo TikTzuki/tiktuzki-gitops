93fa900a271cf85fa70bc8ac878cc0fc4b080e8d
tiktuzki <tiktuzki@gmail.com>
2026-01-31 01:17:36 +0700

    Replace database-secret with keycloak-secret for database credentials

--- startup.sh
+++ startup.sh
@@ -2,8 +2,12 @@ touch ~/.bash_aliases
 cat <<EOF >> ~/.bash_aliases
 alias mk='microk8s'
 alias k='microk8s kubectl'
+alias kst='microk8s kubectl -n kube-system'
 EOF
-apt install neovim
+
+sudo apt install -y neovim
+sudo usermod -a -G microk8s tik
+newgrp microk8s
 microk8s enable dashboard
 
 #microk8s enable hostpath-storage
@@ -14,4 +18,13 @@ sudo mkdir -p /etc/apt/keyrings && curl -fsSL https://packages.openvpn.net/packa
 DISTRO=$(lsb_release -c -s)
 echo "deb [signed-by=/etc/apt/keyrings/openvpn.asc] https://packages.openvpn.net/openvpn3/debian $DISTRO main" | sudo tee /etc/apt/sources.list.d/openvpn-packages.list
 sudo apt update
-sudo apt install openvpn3-client
+sudo apt install -y openvpn3-client
+
+# Fix kubelite error must match public address:
+#sudo nvim /var/snap/microk8s/current/args/kube-apiserver
+#--advertise-address=192.168.1.14
+
+#microk8s kubectl -n kube-system edit daemonset calico-node
+#Inside the calico-node container env:
+#- name: IP_AUTODETECTION_METHOD
+#  value: "interface=eth0"
